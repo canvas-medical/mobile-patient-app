e808b8a3e79d7c131ca06e161fb9b3bf33c7bc4f
Reid Garner <reidbgarner@gmail.com>
2023-11-21 17:15:39 -0600

    moved avatar container into layout.

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -1,10 +1,13 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 import { Slot } from 'expo-router';
-import { Feather } from '@expo/vector-icons';
+import { Feather, FontAwesome } from '@expo/vector-icons';
 import { Screen, DashTabs } from '@components';
 import { g } from '@styles';
 
 const s = StyleSheet.create({
+  container: {
+    paddingTop: g.size(64),
+  },
   drawerButton: {
     position: 'absolute',
     top: g.size(64),
@@ -15,11 +18,27 @@ const s = StyleSheet.create({
     top: g.size(64),
     right: g.size(32),
   },
+  name: {
+    ...g.titleSmall,
+    color: g.white,
+    fontSize: g.size(20),
+    fontWeight: 'bold',
+  },
+  nameAndAvatarContainer: {
+    alignItems: 'center',
+    gap: g.size(8),
+  },
 });
 
 export default function Dashboard() {
   return (
-    <Screen>
+    <Screen style={s.container}>
+      <View style={s.nameAndAvatarContainer}>
+        <FontAwesome name="user-circle-o" size={g.size(96)} color={g.white} />
+        <Text style={s.name}>
+          John Doe
+        </Text>
+      </View>
       <Slot />
       <TouchableOpacity
         style={s.drawerButton}
