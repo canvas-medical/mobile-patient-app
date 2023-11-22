8cc9431fb6334de9def22ba04bce41ab2ab5fd26
Spencer Romberg <33329110+S-Romberg@users.noreply.github.com>
2023-11-21 17:04:17 -0700

    creating menu

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -1,10 +1,41 @@
 import { Screen, DashTabs } from '@components';
+import { SafeAreaView, TouchableOpacity, Text, View, TouchableWithoutFeedback } from 'react-native';
 import { Slot } from 'expo-router';
+import { Feather } from '@expo/vector-icons';
+import { g } from '@styles';
+import { useState } from 'react';
+import Modal from 'react-native-modal';
 
 export default function Dashboard() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <Screen>
-      <Slot />
+      <SafeAreaView>
+        <TouchableOpacity onPress={() => setMenuOpen(!menuOpen)}>
+          <Feather
+            name="menu"
+            size={36}
+            color={g.white}
+            style={{ left: 30 }}
+          />
+        </TouchableOpacity>
+        <Modal
+          style={{ margin: 0, display: 'flex', justifyContent: 'flex-start' }}
+          animationIn="slideInLeft"
+          animationOut="slideOutLeft"
+          isVisible={menuOpen}
+          customBackdrop={(
+            <TouchableWithoutFeedback onPress={() => setMenuOpen(false)}>
+              <View style={{ flex: 1 }} />
+            </TouchableWithoutFeedback>
+          )}
+        >
+          <View style={{ height: g.height * 0.5, width: g.width * 0.5, backgroundColor: g.white }}>
+            <Text>Hello</Text>
+          </View>
+        </Modal>
+        <Slot />
+      </SafeAreaView>
       <DashTabs />
     </Screen>
   );
