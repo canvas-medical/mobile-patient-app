a58f7c9fd2a90f2149c7efabf3f26c09ab1f8e28
Spencer Romberg <33329110+S-Romberg@users.noreply.github.com>
2023-11-30 14:50:34 -0700

    updated request based on Canvas teams response, the bug is still persisting

--- app/(drawer)/invoices.tsx
+++ app/(drawer)/invoices.tsx
@@ -3,100 +3,18 @@ import { ListView } from '@components';
 import { g } from '@styles';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { DocumentResource } from '@interfaces';
+import { useDocumentReferences } from '@services';
 
 export default function Invoices() {
-  // const { data: invoices, isFetching } = useDocumentReferences('invoicefull');
+  const { data: invoices, isFetching } = useDocumentReferences('http://loinc.org|94093-2');
   // const { data: allDocs } = useDocumentReferences();
-  const invoices: {entry: DocumentResource[] } = {
-    entry:
-      [
-        {
-          resource:
-            {
-              resourceType: 'DocumentReference',
-              id: '1cd418c3-3d7b-4b6e-a5d5-75efc9eae27e',
-              status: 'current',
-              type:
-                {
-                  coding:
-                    [
-                      {
-                        system: 'http://loinc.org',
-                        code: '11502-2',
-                        display: 'Laboratory report'
-                      }
-                    ]
-                },
-              category:
-                [
-                  {
-                    coding:
-                      [
-                        {
-                          code: 'labreport'
-                        }
-                      ]
-                  }
-                ],
-              subject:
-                {
-                  reference: 'Patient/cfd91cd3bd9046db81199aa8ee4afd7f',
-                  type: 'Patient'
-                },
-              date: '2023-09-18T00:00:00+00:00',
-              author:
-                [
-                  {
-                    reference: 'Practitioner/4150cd20de8a470aa570a852859ac87e',
-                    type: 'Practitioner'
-                  }
-                ],
-              custodian:
-                {
-                  reference: 'Organization/00000000-0000-0000-0002-000000000000',
-                  type: 'Organization'
-                },
-              content:
-                [
-                  {
-                    attachment:
-                      {
-                        contentType: 'application/pdf',
-                        url: 'https://pdfobject.com/pdf/sample.pdf',
-                      },
-                    format:
-                      {
-                        system: 'http://ihe.net/fhir/ValueSet/IHE.FormatCode.codesystem',
-                        code: 'urn:ihe:iti:xds:2017:mimeTypeSufficient',
-                        display: 'mimeType Sufficient'
-                      }
-                  }
-                ],
-              context:
-                {
-                  encounter:
-                    [
-                      {
-                        reference: 'Encounter/ca757171-c26c-4a83-ad02-d9d44024e01e',
-                        type: 'Encounter'
-                      }
-                    ],
-                  period:
-                    {
-                      start: '2023-09-18T23:38:43.252179+00:00'
-                    }
-                }
-            }
-        }
-      ]
-  };
+  console.log(invoices);
 
   console.log('Invoices: ', invoices);
-  const isFetching = false;
   const icon = <FontAwesome5 name="file-invoice-dollar" size={g.size(36)} color="white" />;
 
   return (
 
-    <ListView clickable icon={icon} items={invoices.entry} title="Invoices" isFetching={isFetching} />
+    <ListView clickable icon={icon} items={invoices?.entry || []} title="Invoices" isFetching={isFetching} />
   );
 }
--- services/document-reference.ts
+++ services/document-reference.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import * as SecureStore from 'expo-secure-store';
 import { getToken } from './access-token';
 
-async function getDocumentReferences(type?: 'labreport'|'imagingreport'|'educationalmaterial'|'invoicefull') {
+async function getDocumentReferences(type?: 'http://loinc.org|94093-2'|'imagingreport'|'educationalmaterial'|'invoicefull') {
   const token = await getToken();
   const patientID = await SecureStore.getItemAsync('patient_id');
   const res = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/DocumentReference?subject=Patient/${patientID}${type ? `?type=${type}` : ''}`, {
@@ -15,7 +15,7 @@ async function getDocumentReferences(type?: 'labreport'|'imagingreport'|'educati
   return res.json();
 }
 
-export function useDocumentReferences(type?: 'labreport'|'imagingreport'|'educationalmaterial'|'invoicefull') {
+export function useDocumentReferences(type?: 'http://loinc.org|94093-2'|'imagingreport'|'educationalmaterial'|'invoicefull') {
   return useQuery({
     queryKey: ['documentReferences'],
     queryFn: () => getDocumentReferences(type),
