e56d2fbf9e1b505df744d3500d7cba710130c49d
Reid Garner <reidbgarner@gmail.com>
2023-12-14 10:23:13 -0600

    dry'd up lab-results service

--- services/lab-results.ts
+++ services/lab-results.ts
@@ -2,37 +2,31 @@ import { useQuery } from '@tanstack/react-query';
 import * as SecureStore from 'expo-secure-store';
 import { getToken } from './access-token';
 
+async function fetchReport(url: string, token: string) {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      accept: 'application/json'
+    }
+  });
+  const data = await response.json();
+  return data.entry?.map((report: any) => report.resource) || [];
+}
+
 async function getLabResults() {
   const token = await getToken();
   const patientID = await SecureStore.getItemAsync('patient_id');
-  const imaging = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/DocumentReference?patient/${patientID}&category=imagingreport`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      accept: 'application/json'
-    }
-  });
-  const lab = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/DocumentReference?patient/${patientID}&category=labreport`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      accept: 'application/json'
-    }
-  });
-  const diagnostics = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/DiagnosticReport?patient=Patient/${patientID}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      accept: 'application/json'
-    }
-  });
-  const imagingReports = await imaging.json();
-  const labReports = await lab.json();
-  const diagnosticsReports = await diagnostics.json();
+  const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
+  const imagingReports = await fetchReport(`${apiUrl}/DocumentReference?patient/${patientID}&category=imagingreport`, token);
+  const labReports = await fetchReport(`${apiUrl}/DocumentReference?patient/${patientID}&category=labreport`, token);
+  const diagnosticsReports = await fetchReport(`${apiUrl}/DiagnosticReport?patient=Patient/${patientID}`, token);
+
   return [
-    ...imagingReports.entry?.map((report: any) => report.resource) || [],
-    ...labReports.entry?.map((report: any) => report.resource) || [],
-    ...diagnosticsReports.entry?.map((report: any) => ({ ...report.resource, date: report.resource.issued })) || [],
+    ...imagingReports,
+    ...labReports,
+    ...diagnosticsReports.map((report: any) => ({ ...report, date: report.issued })),
   ].sort((a: any, b: any) => (
     new Date(b.date).getTime() - new Date(a.date).getTime()
   ));
