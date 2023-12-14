13d3f6f4ff15cb34308e7a392bdf4a0ccb641105
Spencer Romberg <33329110+S-Romberg@users.noreply.github.com>
2023-12-14 15:48:59 -0700

    added lightbult animation when condition card is long pressed

--- app/(tabs)/my-health/_layout.tsx
+++ app/(tabs)/my-health/_layout.tsx
@@ -2,8 +2,8 @@ import { Stack } from 'expo-router';
 
 export default function MyHealthStack() {
   return (
-    <Stack initialRouteName="my-health-dashboard">
-      <Stack.Screen name="my-health-dashboard" options={{ headerShown: false }} />
+    <Stack initialRouteName="my-health">
+      <Stack.Screen name="my-health" options={{ headerShown: false }} />
       <Stack.Screen name="lab-results" options={{ headerShown: false }} />
     </Stack>
   );
--- components/condition-card.tsx
+++ components/condition-card.tsx
@@ -1,5 +1,9 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet, View, Text, Pressable, PixelRatio, Alert
+} from 'react-native';
 import { BlurView } from 'expo-blur';
+import { LightbulbOnSVG } from '@components';
 import { Condition } from '@interfaces';
 import { g } from '@styles';
 
@@ -26,14 +30,26 @@ const s = StyleSheet.create({
   },
 });
 
+const radius = PixelRatio.roundToNearestPixel(130);
+const STROKE_WIDTH = 1;
+
 export function ConditionCard({ condition }: { condition: Condition }) {
+  const [isPressed, setIsPressed] = useState(false);
+
+  // long press time = 500ms
   const {
     code: { text },
     recordedDate,
   } = condition;
 
+  console.log(isPressed);
   return (
-    <View style={s.card}>
+    <Pressable
+      onTouchStart={() => setIsPressed(true)}
+      onTouchEnd={() => setIsPressed(false)}
+      onLongPress={() => Alert.alert('AI stuff will appear here')}
+      style={s.card}
+    >
       <BlurView
         intensity={40}
         tint="light"
@@ -43,11 +59,20 @@ export function ConditionCard({ condition }: { condition: Condition }) {
           <Text style={s.condition}>
             {text}
           </Text>
+          <LightbulbOnSVG
+            lightbulbOn={isPressed}
+            color={g.white}
+            strokeWidth={STROKE_WIDTH}
+            radius={radius}
+            width={g.size(25)}
+            height={g.size(25)}
+            percentageComplete={85}
+          />
           <Text style={s.conditionDate}>
             {new Date(recordedDate).toLocaleDateString()}
           </Text>
         </View>
       </BlurView>
-    </View>
+    </Pressable>
   );
 }
