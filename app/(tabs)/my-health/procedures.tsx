4e790262488dbdf4353a53bac668ba3ff986f18d
Spencer Romberg <33329110+S-Romberg@users.noreply.github.com>
2023-12-21 16:55:24 -0700

    removed unused styles

--- app/(tabs)/my-health/procedures.tsx
+++ app/(tabs)/my-health/procedures.tsx
@@ -1,20 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { useProcedures } from '@services';
 import { ProcedureCard, StackListView } from '@components';
 import { Procedure } from '@interfaces';
 import { g } from '@styles';
-
-const s = StyleSheet.create({
-  label: {
-    ...g.titleXSmall,
-    color: g.white,
-    marginLeft: g.size(4),
-  },
-  scrollSection: {
-    gap: g.size(16),
-  },
-});
 
 export default function Procedures() {
   const { data, isLoading, refetch } = useProcedures();
