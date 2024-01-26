bc8225099adf083e8528a0b3538558679b750c40
Reid Garner <reidbgarner@gmail.com>
2024-01-26 15:28:31 -0600

    added PDF back

--- components/tab-bar.tsx
+++ components/tab-bar.tsx
@@ -1,36 +1,18 @@
 import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-// import { router } from 'expo-router';
-// import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { g } from '@styles';
 
 const s = StyleSheet.create({
-  // bookAppointmentButton: {
-  //   ...g.buttonShadow,
-  //   width: g.ms(72),
-  //   height: g.ms(72),
-  //   borderRadius: g.ms(36),
-  //   backgroundColor: g.secondaryBlue,
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  //   position: 'absolute',
-  //   right: g.ms(12),
-  //   top: -g.ms(84),
-  //   opacity: 0.9,
-  // },
-  buttonContainer: {
+  container: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     borderTopLeftRadius: g.ws(40),
     borderTopRightRadius: g.ws(40),
-    overflow: 'hidden',
     paddingTop: g.hs(8),
     paddingBottom: Platform.OS === 'android' ? g.hs(8) : g.hs(20),
     paddingHorizontal: g.ws(10),
     backgroundColor: g.white,
     elevation: 8,
-  },
-  container: {
     position: 'absolute',
     bottom: 0,
     left: 0,
@@ -63,54 +45,44 @@ export function TabBar({ state, descriptors, navigation }) {
         g.tabBarHeight = e.nativeEvent.layout.height;
       }}
     >
-      <View style={s.buttonContainer}>
-        {state.routes.map((route, index) => {
-          const { options } = descriptors[route.key];
-          const isFocused = state.index === index;
-          const onPress = () => {
-            const event = navigation.emit({
-              type: 'tabPress',
-              target: route.key,
-              canPreventDefault: true,
-            });
-            if (!isFocused && !event.defaultPrevented) {
-              navigation.navigate(route.name, route.params);
-            }
-          };
-          return (
-            <TouchableOpacity
-              key={route.key}
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const isFocused = state.index === index;
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+        return (
+          <TouchableOpacity
+            key={route.key}
+            style={[
+              s.tabButton,
+              { backgroundColor: isFocused ? g.secondaryBlue : g.transparent },
+            ]}
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={route.key}
+            onPress={onPress}
+          >
+            {options.tabBarIcon({ color: isFocused ? options.tabBarActiveTintColor : options.tabBarInactiveTintColor })}
+            <Text
               style={[
-                s.tabButton,
-                { backgroundColor: isFocused ? g.secondaryBlue : g.transparent },
+                s.tabLabel,
+                { color: isFocused ? options.tabBarActiveTintColor : options.tabBarInactiveTintColor }
               ]}
-              accessibilityRole="button"
-              accessibilityState={isFocused ? { selected: true } : {}}
-              accessibilityLabel={options.tabBarAccessibilityLabel}
-              testID={route.key}
-              onPress={onPress}
             >
-              {options.tabBarIcon({ color: isFocused ? options.tabBarActiveTintColor : options.tabBarInactiveTintColor })}
-              <Text
-                style={[
-                  s.tabLabel,
-                  { color: isFocused ? options.tabBarActiveTintColor : options.tabBarInactiveTintColor }
-                ]}
-              >
-                {options.title}
-              </Text>
-            </TouchableOpacity>
-          );
-        })}
-      </View>
-      {/* {state.routes[state.index].name === 'appointments' && (
-        <TouchableOpacity
-          style={s.bookAppointmentButton}
-          onPress={() => router.push('appointments/book-appointment')}
-        >
-          <MaterialCommunityIcons name="calendar-plus" size={g.ms(36)} color={g.white} />
-        </TouchableOpacity>
-      )} */}
+              {options.title}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 }
